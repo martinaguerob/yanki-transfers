457e5a6dab31b590d4f7b733dc7e88cfdad7fd2b
Martin Aguero <maguerob@nttdata.com>
2022-02-26 12:32:43 -0500

    update eureka client and yml

--- src/main/java/com/nttdata/yankitransfers/YankiTransfersApplication.java
+++ src/main/java/com/nttdata/yankitransfers/YankiTransfersApplication.java
@@ -2,8 +2,10 @@ package com.nttdata.yankitransfers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class YankiTransfersApplication {
 
 	public static void main(String[] args) {
